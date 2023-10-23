45c43eb59d8867141815581bc619c6ef7fdd7fc2
Denisus225 <dtitovv@bk.ru>
2023-10-23 17:30:15 +0300

    webpage style изменен мной

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var name = prompt('как вас зовут?');
-var years = prompt('Сколько вам лет?', 100);
+let name = prompt('как вас зовут?');
+let years = prompt('Сколько вам лет?', 100);
 alert('Вам ' + years + ' лет!')
 alert('Ваше имя: ' + name)
 var isAdmin = confirm("Вы - хотите посетить этот сайт?");
